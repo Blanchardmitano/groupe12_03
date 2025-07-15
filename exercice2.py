33fe8091278c9f5dad00ace2fc021a4f80c879cc
Blanchard MITANO <blanchardmitano@gmail.com>
2025-07-15 13:35:34 +0200

    blanc_M

---
+++ exercice2.py
@@ -0,0 +1,9 @@
+# Definition des variables
+nom_produit = "Casque Bluetooth"
+prix = 150.0
+stock = 35
+remise = 0.15 #15%
+#Calcul du prix final
+prix_final = prix * (1 - remise)
+#Affichage
+print()
